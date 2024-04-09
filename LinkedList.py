ba4d95121e9f2cea7af962a0be4b77371d5df4e6
dzakyfaishalariq <dzakyfaishalariq@gmail.com>
2024-04-09 17:11:07 +0700

    Pembelajaran Struktur data dan algoritma

--- LinkedList.py
+++ LinkedList.py
@@ -7,6 +7,11 @@ class Linkedlis:
     def __init__(self):
         self.head = None
 
+    # tambah data awal
+    def tambahDataAwal(self, data):
+        temp = self.head
+        self.head = Node(data)
+        self.head.next = temp
     # tampilkan list
     def printList(self):
         temp = self.head
@@ -23,5 +28,40 @@ class Linkedlis:
             if type(temp.data) == int:
                 total += temp.data
             temp = temp.next
-        return  total
+        return total
+
+    def cariData(self, value):
+        temp = self.head
+        value = int(value)
+        while(temp):
+            if temp.data == value:
+                print("Nilai Ditemukan dengan nilai : {}".format(temp.data))
+                break
+            temp = temp.next
+        return
+    def hapusData(self, value):
+        # menghapus head
+        temp = self.head
+        if temp is not None:
+            if temp.data == value:
+                self.head = temp.next
+                temp = None
+                return
+        # mencari nilai value di list
+        while temp is not None:
+            if temp.data == value:
+                break
+            prev = temp
+            temp = temp.next
+        # jika value tidak ada di list
+        if temp == None:
+            return
+        # ubah link node dari linkedlist
+        prev.next = temp.next
+
+        temp = None
+
+
+
+
 
--- main.py
+++ main.py
@@ -5,12 +5,22 @@ if __name__ == "__main__":
     list.head = Node(1)
     kedua = Node(2)
     ketiga = Node(3)
+    keempat = Node("Hllo")
 
     # menyambungkan dengan node
     list.head.next = kedua
     kedua.next = ketiga
+    ketiga.next = keempat
     # tampilkan lis
     list.printList()
     # tampilkan jumlah list
     print(list.sumList())
+    list.cariData(3)
+    print(list.hapusData(2))
+    list.printList()
+
+    list.tambahDataAwal(40)
+    list.tambahDataAwal(41)
+    list.tambahDataAwal(42)
+    list.printList()
     print("Pogram Berhasil Dijalankan")
